eb5086c71fe2e692c9a2c5aabc4702aee59fda14
AlexTing1 <alexting6366@gmail.com>
2021-10-20 15:42:34 -0700

    added comments to App.jsx

--- client/src/Components/App.jsx
+++ client/src/Components/App.jsx
@@ -1,17 +1,14 @@
 import React, { useState } from 'react';
 import {
-  BrowserRouter as Router, Switch, Route, Link,
+  BrowserRouter as Router, Switch, Route,
 } from 'react-router-dom';
 import Start from './Start';
 import TwitterAnalysis from './TwitterAnalysis';
 import css from './css/app.css';
 
+//  Initialization of the App. App.jsx maps the router paths of the web application.
 function App() {
   const [id, setId] = useState('1232319080637616128');
-
-  function demoOnClick() {
-    console.log('test');
-  }
 
   return (
 
